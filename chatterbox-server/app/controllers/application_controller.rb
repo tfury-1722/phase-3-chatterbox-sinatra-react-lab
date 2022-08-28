0cd22376d4698860d9d56fba1b57fe2cc050f7f8
christopher <cdnance1829@gmail.com>
2022-08-27 21:26:11 -0400

    Completed

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -3,4 +3,25 @@ class ApplicationController < Sinatra::Base
   
   # Add routes
   
+  get '/messages'do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do
+    comment = Message.create(body: params[:body], username: params[:username])
+    comment.to_json
+  end
+
+  patch '/messages/:id' do
+    updated_comment = Message.find(params[:id])
+    updated_comment.update(body: params[:body])
+    updated_comment.to_json
+  end
+
+  delete "/messages/:id" do
+    bad_comment = Message.find(params[:id])
+    bad_comment.destroy
+    bad_comment.to_json
+  end
 end
